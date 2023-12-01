884176c47e48d539f0b96aac8138697a4251e315
ogbinar <myk.ogbinar@yahoo.com.ph>
2023-12-01 08:54:34 +0800

    j

--- HW4/webcam2gstreamer.py
+++ HW4/webcam2gstreamer.py
@@ -1,3 +1,19 @@
+"""
+setup ffmepg locally
+
+export GST_DEBUG="udpsrc:6"
+gst-launch-1.0 udpsrc port=5000 ! decodebin ! appsink name=myappsink
+
+export GST_DEBUG="udpsrc:6"
+
+gst-launch-1.0 udpsrc port=5000 ! parse-rtp ! rtph264depay ! decodebin ! videoconvert ! jpegenc ! appsink name=myappsink
+
+This pipeline will receive UDP packets containing H.264 encoded video frames, decode them, convert them to JPEG format, and send them to an appsink named myappsink. You can then connect to the appsink using Python and forward the JPEG images to the PyTorch Inference Server REST API.
+
+Here's an example of how to connect to the appsink and forward the JPEG images to the PyTorch Inference Server REST API using Python:
+
+"""
+
 import dash
 from dash import dcc
 from dash import html
@@ -9,6 +25,9 @@ import logging
 import atexit
 import os
 import tempfile
+import subprocess
+import numpy as np
+logging.basicConfig(level=logging.DEBUG)
 
 class VideoCamera:
     def __init__(self):
@@ -47,24 +66,31 @@ def capture_video():
             break
 
 def encode_video(frame):
-    # Create a temporary file to store the encoded frame
-    with tempfile.NamedTemporaryFile(suffix='.jpg',dir=".") as f:
-        # Save the encoded frame to the temporary file
-        f.write(frame)
-        f.flush()
-        # Change file permissions to allow FFmpeg read access
-        os.chmod(f.name, 0o666)
-        # Pass the temporary file path to FFmpeg's input stream
-        stream = ffmpeg.input(f.name)
+    # Encode the frame as JPEG
+    #print(type(frame))
+    #print(frame.size)
+    # Convert the bytes to a numpy array
+    frame = np.frombuffer(frame, dtype=np.uint8)
+    # Decode the numpy array as an image
+    frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+    _, encoded_frame = cv2.imencode('.jpg', frame)
 
-        # Encode and stream the video frame
-        stream = ffmpeg.output(stream, 'rtp://23.94.57.130:5000', format='rtp')
-        ffmpeg.run(stream)
+    # Create a subprocess for FFmpeg
+    process = (
+        ffmpeg
+        .input('pipe:0')
+        .output('rtp://23.94.57.130:5000', format='rtp')
+        .run_async(pipe_stdin=True, pipe_stdout=True, pipe_stderr=True)
+    )
 
-        # Delete the temporary file
-        #os.remove(f.name)
+    # Pass the encoded frame to FFmpeg's input stream
+    process.stdin.write(encoded_frame.tobytes())
+    process.stdin.close()
 
-        logging.info("Encoded and streamed video frame")
+    # Wait for FFmpeg to finish
+    process.wait()
+
+    logging.info("Encoded and streamed video frame")
 
 app.layout = html.Div([
     html.Video(id='webcam-feed'),
