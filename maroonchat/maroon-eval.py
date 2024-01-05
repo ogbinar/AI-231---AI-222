4f67a33df1943070b211fc851cffe75222a10001
ogbinar <myk.ogbinar@yahoo.com.ph>
2024-01-05 12:08:51 +0800

    Update maroon-eval.py
    edited to make it easier to edit

--- maroonchat/maroon-eval.py
+++ maroonchat/maroon-eval.py
@@ -12,16 +12,16 @@ from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.prompts import PromptTemplate
 from langchain.schema import StrOutputParser
 from langchain_community.llms import HuggingFaceHub
+from langchain import HuggingFacePipeline
 from langchain.chains import LLMChain
 import numpy as np
 from sentence_transformers import SentenceTransformer
 import jellyfish
 
-embedding_model_name="BAAI/bge-small-en-v1.5"
+embedding_model_name = "BAAI/bge-small-en-v1.5"
 embedding_model = SentenceTransformer(embedding_model_name)
 # Set HuggingFaceHub API token
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
-
 
 
 def calculate_similarity(reference, candidate):
@@ -50,30 +50,62 @@ def calculate_similarity(reference, candidate):
     # Normalized Levenshtein Score (1-0)
     # Levenshtein: Counts edits, good for word order and typos, strict on word choice.
     levenshtein_score = 1 - (jellyfish.levenshtein_distance(str(reference), str(candidate)) / max(len(str(reference)), len(str(candidate))))
-    
+
     # Normalized Jaro-Winkler Score (1-0)
     # Jaro-Winkler: Forgives some errors, good for names or addresses, balances exactness and flexibility.
     jaro_winkler_score = jellyfish.jaro_winkler_similarity(str(reference), str(candidate))
 
     return similarity_embeddings, levenshtein_score, jaro_winkler_score
 
+
 # Read the Excel file
 df = pd.read_excel("MAROON CHAT Q&A.xlsx", sheet_name="Public")
 
 # Define model path
-model_path = "merged_model/ggml-model-Q4_0.gguf"
-
+#model_path = "combined_merged_model/ggml-model-Q4_0.gguf"
+#model_path = "combined_merged_model/ggml-model-q4_0.gguf"
 # Initialize LlamaCpp
-llm = LlamaCpp(
-    model_path=model_path,
-    temperature=0.1,
-    max_tokens=256,
-    n_gpu_layers=10000,
-    top_p=1,
-    n_ctx=4096,
-    callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
-    verbose=True,
+#llm = LlamaCpp(
+#    model_path=model_path,
+#    temperature=0.1,
+#    max_tokens=256,
+#    n_gpu_layers=10000,
+#    top_p=1,
+#    n_ctx=4096,
+#    callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
+#    verbose=True,
+#)
+ 
+import torch
+from transformers import BitsAndBytesConfig
+quantization_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_compute_dtype=torch.float16,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_use_double_quant=True,
 )
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+device_map = {"":0}
+
+# indicate where your model and tokenizers are 
+model_dir = "/home/michel/.cache/huggingface/hub/models--mistralai--Mistral-7B-Instruct-v0.2/snapshots/b70aa86578567ba3301b21c8a27bea4e8f6d6d61"
+tokenizer_dir = "combined_merged_model"
+model_4bit = AutoModelForCausalLM.from_pretrained( model_dir, device_map=device_map,quantization_config=quantization_config, )
+tokenizer = AutoTokenizer.from_pretrained(tokenizer_dir)
+pipeline = pipeline(
+        "text-generation",
+        model=model_4bit,
+        tokenizer=tokenizer,
+        use_cache=True,
+        device_map=device_map,
+        max_length=256,
+        eos_token_id=tokenizer.eos_token_id,
+        pad_token_id=tokenizer.eos_token_id,
+) 
+from langchain.llms.huggingface_pipeline import HuggingFacePipeline
+
+llm = HuggingFacePipeline(pipeline=pipeline)
+
 
 # Define HuggingFaceHub repo ID
 repo_id = "HuggingFaceH4/zephyr-7b-beta"
@@ -144,13 +176,12 @@ for index, row in df.iterrows():
         print("No float value found in the text.")
 
     similarity_embeddings, levenshtein_score, jaro_winkler_score = calculate_similarity(gt, cb_answer)
- 
+
     """
     Embeddings: Understands meaning, good for similar ideas with different words.
     Levenshtein: Counts edits, good for word order and typos, strict on word choice.
     Jaro-Winkler: Forgives some errors, good for names or addresses, balances exactness and flexibility.
     """
-
 
     # Append answer pair to the list
     answer_pairs.append({
@@ -161,8 +192,6 @@ for index, row in df.iterrows():
         "levenshtein_score": levenshtein_score,
         "jaro-wingkler_score": jaro_winkler_score,
         "embeddings_score": similarity_embeddings
-
-
     })
 
 # Create dataframe from answer pairs
